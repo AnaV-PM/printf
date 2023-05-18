54ce8ad8243bc953ad193d22fbdff51f400d8323
argenismiranda <argenismiranda@gmail.com>
2023-05-18 12:05:41 -0700

    printf-patner-project

--- functions.c
+++ functions.c
@@ -1,8 +1,10 @@
 #include "main.h"
 
+/************************* PRINT CHAR *************************/
+
 /**
  * print_char - Prints a char
- * @types: List a of arguments
+ * @types: Lista of arguments
  * @buffer: Buffer array to handle print
  * @flags:  Calculates active flags
  * @width: Width
@@ -16,10 +18,11 @@ int print_char(va_list types, char buffer[],
 	char c = va_arg(types, int);
 
 	return (handle_write_char(c, buffer, flags, width, precision, size));
-	}
+}
+/************************* PRINT A STRING *************************/
 /**
  * print_string - Prints a string
- * @types: List a of arguments
+ * @types: Lista of arguments
  * @buffer: Buffer array to handle print
  * @flags:  Calculates active flags
  * @width: get width.
@@ -70,7 +73,8 @@ int print_string(va_list types, char buffer[],
 	}
 
 	return (write(1, str, length));
-	}
+}
+/************************* PRINT PERCENT SIGN *************************/
 /**
  * print_percent - Prints a percent sign
  * @types: Lista of arguments
@@ -93,6 +97,7 @@ int print_percent(va_list types, char buffer[],
 	return (write(1, "%%", 1));
 }
 
+/************************* PRINT INT *************************/
 /**
  * print_int - Print int
  * @types: Lista of arguments
@@ -136,6 +141,7 @@ int print_int(va_list types, char buffer[],
 	return (write_number(is_negative, i, buffer, flags, width, precision, size));
 }
 
+/************************* PRINT BINARY *************************/
 /**
  * print_binary - Prints an unsigned number
  * @types: Lista of arguments
